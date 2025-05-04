5caf48dadf43ee1ea5f5ca55bdc5d6327de133bb
Alexandre Vallon <81435330+Al-vallon@users.noreply.github.com>
2025-05-04 10:41:05 +0200

    initial commit

---
+++ static/main.js
@@ -0,0 +1,39 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const statusText = document.getElementById('motion');
+
+const socket = io("http://localhost:5000");
+
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    status.textContent = data.motion ? "Mouvement détecté !" : "Aucun";
+    status.style.color = data.motion ? "red" : "green";
+
+    if (data.frame) {
+        const uint8Arr = new Uint8Array(Object.values(data.frame));
+        const blob = new Blob([uint8Arr], { type: 'image/jpeg' });
+        document.getElementById('motionFrame').src = URL.createObjectURL(blob);
+    }
+};
+
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+    video.srcObject = stream;
+});
+
+setInterval(() => {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        canvas.toBlob(blob => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const arrayBuffer = reader.result;
+            socket.emit("message", arrayBuffer);
+        };
+        reader.readAsArrayBuffer(blob);
+        }, 'image/jpeg');
+    }
+}, 500);
